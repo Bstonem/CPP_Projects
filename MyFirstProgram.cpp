1de9b94a6662a4b751b3e12bb01183147b83b0b6
Bstonem <blystonem@gmail.com>
2025-01-29 11:46:01 -0600

    // This is the final version of MyFirstProgram

---
+++ MyFirstProgram.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(){
+
+    int year = 2077;
+    float population = 9.5;
+    double popGrowth = 1.0578;
+    char epoch = 'V';
+    bool aiOverlords = true;
+    std::string salutation = "Hello, World!";
+
+    std::cout << "Integer: " << year << std::endl;
+    std::cout << "Float: " << population << std::endl;
+    std::cout << "Double: " << popGrowth << std::endl;
+    std::cout << "Character: " << epoch << std::endl;
+    std::cout << "Boolean: " << aiOverlords << std::endl;
+    std::cout << "String: " << salutation << std::endl;
+
+   
+
+    return 0;
+}
